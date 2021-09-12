e6be5d616181290ddfa7312e43719f20a89f960c
Leonardo Guglielmi <leonardo.guglielmi@stud.unifi.it>
2021-09-12 18:29:59 +0200

    removed directory cmake-build-debug

--- cmake-build-debug/test/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Leo/UniFi/anno 1/Laboratorio di programmazione/MyNotes/test/googletest/googlemock
-# Build directory: C:/Users/Leo/UniFi/anno 1/Laboratorio di programmazione/MyNotes/cmake-build-debug/test/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- cmake-build-debug/test/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Leo/UniFi/anno 1/Laboratorio di programmazione/MyNotes/test/googletest/googletest
-# Build directory: C:/Users/Leo/UniFi/anno 1/Laboratorio di programmazione/MyNotes/cmake-build-debug/test/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
